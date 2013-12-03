062c6305e5c08ac4ca43a9aff57729178cca8140
Salem Abei <Salem@Salems-MacBook-Pro.local>
2013-12-03 14:07:54 +0100

    Todo und Done Liste getrennt

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,8 @@ class TasksController < ApplicationController
   # GET /tasks
   # GET /tasks.json
   def index
-    @tasks = Task.all
+    @done = Task.where(done: true)
+    @todo = Task.where(done: false)
   end
 
   # GET /tasks/1
