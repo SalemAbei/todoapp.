bbd5e65ba235b557ce3020b18527f019bb9ca8c5
Salem Abei <Salem@salems-mbp.wlanp.hwr-berlin.de>
2013-12-10 17:49:06 +0100

    distance_from_now_in_days Methode in Task eingefügt

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,6 +3,11 @@ class Task < ActiveRecord::Base
 	def is_delayed?
 	 	self.deadline < Date.today
 	end
+	
+	def distance_from_now_in_days
+		(self.deadline - Date.today).to_i     
+	end
+
 	validates :name, presence: true
 	validates :deadline, presence: true
 	validates :duration, presence: true, numericality: true 
