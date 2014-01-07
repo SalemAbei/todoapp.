bdfc96ae6afe5b731ef3cc34304a8ce67b5dc17c
Salem Abei <Salem@salems-mbp.wlanp.hwr-berlin.de>
2014-01-07 16:02:30 +0100

    Username zu User hinzugefügt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
+
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :tasks
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  validates :username, presence:  true, uniqueness: true
 end
